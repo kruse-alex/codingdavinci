a799b31d365c053b6651d9a9ab05c404cddf2611
Alex Kruse <kruse-alex@outlook.de>
2016-10-27 11:26:13 +0200

    Update server.R

--- server.R
+++ server.R
@@ -18,17 +18,14 @@ require(data.table)
 shinyServer(
   function(input, output, session){
     
-    
-    setwd("C:/Users/akruse/Documents/R/CDV/CDV_final/")
-    ger_plz <- readOGR(dsn = ".", layer = "gerplz2")
-    schools <- read.table("SchulenPLZ.csv", header = T, sep = ";")
+    #setwd("C:/Users/akruse/Documents/R/CDV/CDV/")
+    ger_plz <- readOGR(dsn = ".", layer = "gerplz3")
+    schools <- read.table("SchulenPLZfull2.csv", header = T, sep = ";")
+    schools2 <- read.table("SchulenPLZfull3.csv", header = T, sep = ";")
     artmus <- read.table("museen_liste2.txt", header = T, sep = ",")
-    mydata <- read.table("mydata.csv", header = T, sep = ";")
-    
-    ####observer is used to maintain the circle size. 
+    mydata <- read.table("mydatafull.csv", header = T, sep = ";")
     
     observe({
-      #####this connects selectInput and assigns the radius value
       kennzahl_label <- input$kennzahl
       radius <- ger_plz@data[[kennzahl_label]]
       
@@ -38,50 +35,32 @@ shinyServer(
         domain = radius
       )
       
-      # load custom icon
-      #museumIcon <- makeIcon(
-       # iconUrl = "museum_icon.png",
-        #iconWidth = 20, iconHeight = 20,
-        #popupAnchorX = 0,
-        #popupAnchorY = 0
-        #iconAnchorX = 22, iconAnchorY = 94,
-        #shadowUrl = "http://leafletjs.com/docs/images/leaf-shadow.png",
-        #shadowWidth = 50, shadowHeight = 64,
-        #shadowAnchorX = 4, shadowAnchorY = 62
-      #)
-      
-      # load custom icon
-      #schoolIcon <- makeIcon(
-       # iconUrl = "school_icon.png",
-        #iconWidth = 20, iconHeight = 20,
-        #popupAnchorX = 0,
-        #popupAnchorY = 0
-        #iconAnchorX = 22, iconAnchorY = 94,
-        #shadowUrl = "http://leafletjs.com/docs/images/leaf-shadow.png",
-        #shadowWidth = 50, shadowHeight = 64,
-        #shadowAnchorX = 4, shadowAnchorY = 62
-      #)
-      
       # create pop ups
       state_popup <- paste0("<strong>Postleitzahl: </strong>", 
                             ger_plz@data$note, 
-                            "<br><strong>Gruppenbuchungen: </strong>", 
+                            "<br><strong>Musuemsausflüge: </strong>", 
                             ger_plz@data$zipcnt,
-                            "<br><strong>Sch�ler: </strong>", 
+                            "<br><strong>Schüler: </strong>", 
                             ger_plz@data$stcnt,
-                            "<br><strong>Sch�ler pro Gruppenbuchung: </strong>", 
+                            "<br><strong>Museumsausflüge/Schüler: </strong>", 
                             ger_plz@data$zpcnt_s)
       
       # create pop ups
-      schule_popup <- paste0("<strong>Schule: </strong>", 
-                            schools$Schulname, 
-                            "<br><strong>Sch�ler: </strong>", 
-                            schools$Ergebnis)
-      
+      schule_popup <- paste0("<strong>Schüler: </strong>", 
+                            schools2$Schueler,
+                            "<br><strong>Grundschulen: </strong>", 
+                            schools2$gru,
+                            "<br><strong>Stadtteilschulen: </strong>", 
+                            schools2$std,
+                            "<br><strong>Sonderschulen: </strong>", 
+                            schools2$son,
+                            "<br><strong>Gymnasien: </strong>", 
+                            schools2$gym)
+                   
       # create pop ups
       artmus_popup <- paste0("<strong>Museum: </strong>", 
                              artmus$museums_name, 
-                             "<br><strong>Gruppenbuchungen: </strong>", 
+                             "<br><strong>Musuemsausflüge: </strong>", 
                              artmus$count)
       
       
@@ -92,81 +71,90 @@ shinyServer(
         ger_plz.map %>% addPolygons(stroke = T, smoothFactor = 0.2, fillOpacity = 0.7, color = "black", weight = 2,
                                     fillColor = ~pal(radius),
                                     popup = state_popup) %>%
-          addCircles(lng = schools$longitude, lat = schools$latitude, popup = schule_popup, fillOpacity = 100, fillColor = "#0000CD", stroke = F, radius = 100, group = "locations") %>%
-          #addMarkers(lng = schools$longitude, lat = schools$latitude, popup = schools$Schulname,ic, group = "locations") %>%
-          #addMarkers(lng = artmus$longitude, lat = artmus$latitude, popup = artmus$museums_name, group = "locations_2", icon = museumIcon)
-          addCircles(lng = artmus$longitude, lat = artmus$latitude, popup = artmus_popup, fillOpacity = 100, fillColor = "#000000", stroke = F, radius = 200, group = "locations_2")
-        
-      
+          addCircles(lng = artmus$longitude, lat = artmus$latitude, popup = artmus_popup, fillOpacity = 100, fillColor = "#000000", stroke = F, radius = 170, group = "locations_2") %>%
+          addCircles(lng = schools2$longitude, lat = schools2$latitude, popup = schule_popup, fillOpacity = 100, fillColor = "#0000CD", stroke = F, radius = 100, group = "locations")
         
       })
       
       output$plzPlot <- renderPlot({
         
         # load data
-        mydata <- read.table("mydata.csv", header = T, sep = ";")
+        mydata <- read.table("mydatafull.csv", header = T, sep = ";")
         
         # Render a barplot
-        check <- filter(mydata, PLZ == paste(input$plz))
+        empty <- mydata %>% group_by(Schulform) %>% summarise(Buchungen = n())
+        empty$Buchungen[empty$Buchungen > 0] <- 0
         
+        check <- filter(mydata, PLZ == paste(input$plz))
         check <- check %>% group_by(Schulform) %>% summarise(Buchungen = n())
+        
+        check <- rbind(empty,check)
+        check <- check %>% group_by(Schulform) %>% summarise(Buchungen = sum(Buchungen))
+        
         ggplot(check, aes(x = Schulform, y = Buchungen)) +
           geom_bar(stat = "identity", aes(fill = Schulform)) +
           theme_monokai_full() +
-          ggtitle("") +
-          #scale_fill_manual(values = pal("monokai")) +
-          theme(axis.text.x = element_text(angle = 90, hjust = 1))
+          ggtitle("Museumsausflüge pro Schulform") +
+          theme(legend.position="none")
       
       })
       
-      
-      # display 10 rows initially
+      output$schulPlot <- renderPlot({
+        
+        # Render a barplot
+        empty <- schools %>% group_by(Schulform) %>% summarise(Schueler = sum(Schueler))
+        empty$Schueler[empty$Schueler > 0] <- 0
+        
+        check1 <- filter(schools, Schule.PLZ == paste(input$plz))
+        check1 <- check1 %>% group_by(Schulform) %>% summarise(Schueler = sum(Schueler))
+        
+        check1 <- rbind(empty,check1)
+        check1 <- check1 %>% group_by(Schulform) %>% summarise(Schueler = sum(Schueler))
+        
+        ggplot(check1, aes(x = Schulform, y = Schueler)) +
+          geom_bar(stat = "identity", aes(fill = Schulform)) +
+          theme_monokai_full() +
+          ggtitle("Schüler pro Schulform") +
+          theme(legend.position="none")
+        
+      })
       
       check1 <- filter(schools, Schule.PLZ == paste(input$plz))
-      check1 <- select(check1, Schulname, Schule.Strasse, Schule.Hausnr, Schule.PLZ, Ergebnis)
-      check1$Strasse <- paste(check1$Schule.Strasse,check1$Schule.Hausnr, sep = " ")
-      check1 <- select(check1, Schulname, Strasse, Schule.PLZ, Ergebnis)
-      colnames(check1) <- c("Schulname","Strasse","PLZ","Sch�ler")
+      check1 <- select(check1, Schulname, Schulform, Schule.PLZ, Schueler)
+      colnames(check1) <- c("Schule","Schulform","PLZ","Schüler")
       
       output$ex1 <- renderTable(
         as.data.table(check1, options = list(pageLength = 25))
       )
-      
+
     })
     
     
     # Use a separate observer to recreate the legend as needed.
     observe({
-      #####this connects selectInput and assigns the radius value
       kennzahl_label <- input$kennzahl
       radius <- ger_plz@data[[kennzahl_label]]
       
-      # continous colour palette
-      pal <- colorNumeric(
-        palette = "RdYlGn",
-        domain = radius
-      )
+      
+      
       proxy <- leafletProxy("ger_plz.map", data = ger_plz)
       
-      # Remove any existing legend, and only if the legend is
-      # enabled, create a new one.
+      # legend
       proxy %>% clearControls()
-      if (input$legend) {
+      if (input$legende) {
         
-        proxy %>% addLegend(position = "bottomright", title = ifelse(input$kennzahl == "stcnt", paste("Sch�ler"), ifelse(input$kennzahl == "zipcnt", paste("Buchungen"), paste("Sch�ler pro Gruppenbuchung"))),
+        # continous colour palette
+        pal <- colorNumeric(
+          palette = "RdYlGn",
+          domain = radius
+        )
+        
+        proxy %>% addLegend(position = "bottomright", title = ifelse(input$kennzahl == "stcnt", paste("Schüler"), ifelse(input$kennzahl == "zipcnt", paste("Musuemsausflüge"), paste("Musuemsausflüge/Schüler"))),
                             pal = pal, values = ~radius)
         
-        proxy %>% addLegend(
-          position = 'topright',
-          colors = c("#0000CD","#000000"),
-          labels = c("Schulen","Museen"),
-          title = '',
-          opacity = 100
-        )
+        proxy %>% addLegend(position = 'topright',colors = c("#0000CD","#000000"),labels = c("Schulen","Museen"),title = '',opacity = 100)
+        
       }
     })         
-    
 
-    
-    
   })
